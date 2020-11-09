73d0a87d9d819ca7c2b21e396d9d07977ee30d29
Otavio <ehren.dev.mail@gmail.com>
2020-11-09 18:52:03 -0300

    Initial commit

---
+++ install-docker.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+echo "Run script prepended with 'sudo' as in $ sudo bash install-docker.sh"
+echo ""
+echo ""
+echo "Uninstalling possible existing docker versions..."
+echo ""
+echo ""
+sudo apt-get remove docker docker-engine docker.io containerd runc
+echo "Updating repos..."
+echo ""
+echo ""
+sudo apt-get update
+echo ""
+echo ""
+echo "Installing required packages..."
+echo ""
+echo ""
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+echo ""
+echo ""
+echo "Adding Docker's GPG key..."
+echo ""
+echo ""
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo ""
+echo ""
+echo "Will output 'Docker Release (CE deb) if everything's ok:"
+echo ""
+echo ""
+sudo apt-key fingerprint 0EBFCD88
+echo ""
+echo ""
+echo "Adding Docker's repository to local repositories..."
+echo ""
+echo ""
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+echo ""
+echo ""
+echo "Updating local repositories & installing required packages..."
+echo ""
+echo ""
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+echo ""
+echo ""
+echo "Running Docker's hello-world..."
+echo ""
+echo ""
+sudo docker run hello-world
+echo ""
+echo ""
+echo "Seems that docker was successfully installed, nice!"
